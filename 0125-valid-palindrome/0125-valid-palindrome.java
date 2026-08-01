ec8c4cd7b11012d0b64d2d959b3d75145c663b38
PurvaSarode03 <purvasarode20@gmail.com>
2026-08-01 23:26:36 +0530

    Time: 2 ms (99.31%), Space: 44.1 MB (92.04%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+	 int left=0;
+	 int right=s.length()-1;
+	 
+	 while(left<right){
+		 char l=s.charAt(left);
+		 char r=s.charAt(right);
+		 
+		 if(!isAlPhaNumber(l)) {
+			 left++;
+		 }
+		 else if(!isAlPhaNumber(r)) {
+			 right--;
+			 
+		 }
+		 else {
+			 if(Character.toLowerCase(r)!=Character.toLowerCase(l)) {
+				 return false;
+			 }
+			 left++;
+			 right--;
+		 }
+		 
+		 
+	 }
+	 return true;
+	    
+	 }
+	 
+	 private boolean  isAlPhaNumber(char c) {
+		 return (c>='a' && c<='z') || 
+				 (c>='A' && c<='Z') ||
+				 (c>='0' && c<='9');
+	 }
+}
