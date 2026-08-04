2bac37367a4c232404372cd57a175d1c64f2ec83
PurvaSarode03 <purvasarode20@gmail.com>
2026-08-04 20:34:28 +0530

    Time: 5 ms (98.08%), Space: 43.2 MB (30.04%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,31 +1,27 @@
 class Solution {
 public static int countSubstrings(String s) {
 	int count=0;
-	int left=0;
-	while(left<s.length()) {
+	int n=s.length();
+	
+	for(int i=0;i<n;i++) {
+		count+=ispalindrome(s, i, i); //for odd length substring
+		count+=ispalindrome(s, i, i+1); //for even lenth substring
 		
-	
-	for(int right=left;right<s.length();right++) {
-		if(ispalindrome(s, left, right)) {
-			count++;
-		}
-	}
-	
-     left++;   
 	}
 return count;
-
 }
 
-private static boolean ispalindrome(String s ,int left,int right) {
-	while(left<right) {
-		if(s.charAt(right)!=s.charAt(left))
-			return false;
-		left++;
-		right--;
+private static int  ispalindrome(String s ,int left,int right) {
+	int count=0;
+ 
+		while(left>=0 && right<s.length() 
+        && s.charAt(right)==s.charAt(left)) {
+		count++;
+		left--;   //expanding phase
+		right++;
 	}
 	
-	return true;
+	return count;
 	
 }
 }
