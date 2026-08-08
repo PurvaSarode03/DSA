224a843d188893f0058389bbe491ee23a1aee3de
Purva Sarode <purvasarode20@gmail.com>
2026-08-08 20:58:00 +0530

    Time: 19 ms (30.45%), Space: 44.8 MB (29.86%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,41 +1,37 @@
 class Solution {
-   	 public static boolean checkInclusion(String s1, String s2) {
-	     
-		 int [] freq1= new int[26];
-		 int [] freq2= new int[26];
-		 
-		 for(char c:s1.toCharArray()) {
-			 freq1[c-'a']++;
-		 }
-		 
-		 int left=0;
-		 
-		 for(int right=0;right<s2.length();right++) {
-			 freq2[s2.charAt(right)-'a']++;
-			 
-			 if(right-left+1>s1.length()) {
-				 char ch=s2.charAt(left);
-				 freq2[ch-'a']--;
+   	public static boolean checkInclusion(String s1, String s2) {
+		if(s1.length()>s2.length())return false;
+		
+		HashMap<Character, Integer> map= new HashMap<Character, Integer>();
+		
+		for(char c:s1.toCharArray()) {
+			map.put(c, map.getOrDefault(c, 0)+1);
+			}
+		
+		int left=0,count=s1.length();
+		
+		for(int right=0;right<s2.length();right++) {
+			char ch=s2.charAt(right);
+			
+			int val=map.getOrDefault(ch, 0);
+			//If ch exists in the map return its value.If ch does not exist return 0.
+			if(val>0)count--;
+			map.put(ch, val-1);//if not present in map then its value is -1
+			
+			if(count==0) return true;
+			
+			if(right-left+1==s1.length()) {
+				char leftchar=s2.charAt(left);
+				
+				int leftval=map.get(leftchar);
+				if(leftval>=0) count++;
+				map.put(leftchar, leftval+1);
 				left++;
-			 }
-			 
-			 if(right-left+1==s1.length()) {
-				 boolean ismatch=true;
-				 for(int i=0;i<26;i++) {
-					 if(freq1[i]!=freq2[i]) {
-						 ismatch=false;
-						 break;
-					 }
-						
-						 
-				 }
-				 
-				 if(ismatch) return true;
-			 }
-			 
-			 
-		 }
-		 
-		 return false;
-	    }
+			}
+			
+		}
+		
+	
+	return false;
+	}
 }
