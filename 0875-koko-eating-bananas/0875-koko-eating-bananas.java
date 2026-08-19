62d4cf558fab63a9736fa76cbd2d0993c8a4892b
Purva Sarode <purvasarode20@gmail.com>
2026-08-19 22:33:59 +0530

    Time: 6 ms (99.67%), Space: 48 MB (32.61%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low=1;
+        int high=0;
+
+        for(int pile:piles){
+            high=Math.max(pile,high);
+        }
+        int ans=high;
+        while(low<=high){
+            int mid= low+(high-low)/2;
+
+            if(canEat(piles,h,mid)){
+                ans=mid;
+                high=mid-1;//bcoz we want min speed
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+
+        private boolean canEat(int[] pile,int h,int k){
+            long hours=0;
+            for(int piles:pile){
+                hours+=(piles+k-1)/k;
+            }
+              return hours<=h;
+        }
+        
+    
+}
