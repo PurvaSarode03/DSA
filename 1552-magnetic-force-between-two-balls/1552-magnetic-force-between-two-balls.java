37542e5fba3c22bf1798a044a59442b0908d8aaf
Purva Sarode <purvasarode20@gmail.com>
2026-08-25 00:19:17 +0530

    Time: 47 ms (46.72%), Space: 79.3 MB (25.32%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
@@ -1,46 +1,34 @@
 class Solution {
    public int maxDistance(int[] position, int m) {
-
-    Arrays.sort(position);
-
-    int low = 1;
-    int high = position[position.length - 1] - position[0];
-
-    int ans = 0;
-
-    while (low <= high) {
-
-        int mid = low + (high - low) / 2;
-
-        if (canPlace(position, m, mid)) {
-            ans = mid;
-            low = mid + 1;
-        } 
-        else {
-            high = mid - 1;
+   int n=position.length;
+   Arrays.sort(position);
+   int low=1;
+   int high=position[n-1]-position[0];
+    int ans=0;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(canReach(position,m,mid)){
+            ans=mid;
+            low=mid+1;
+        }else{
+            high=mid-1;
         }
-    }
 
-    return ans;
+    
+    }
+return ans;
+
 }
-private boolean canPlace(int[] position, int m, int distance) {
 
-    int balls = 1;
-    int lastPosition = position[0];
-
-    for (int i = 1; i < position.length; i++) {
-
-        if (position[i] - lastPosition >= distance) {
-
-            balls++;
-            lastPosition = position[i];
-        }
-
-        if (balls == m) {
-            return true;
+private boolean canReach(int[] arr,int m,int mid){
+    int count=1;
+    int last=arr[0];
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]-last>=mid){
+            count++;
+            last=arr[i];
         }
     }
-
-    return false;
+    return count>=m;
 }
 }
